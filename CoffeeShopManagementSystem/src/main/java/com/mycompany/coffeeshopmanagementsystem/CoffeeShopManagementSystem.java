e0610a87356897bdb1d5bf85da47c37effd80ce5
OracleSkies <153785033+OracleSkies@users.noreply.github.com>
2024-12-11 11:59:04 +0800

    Branch setup

---
+++ CoffeeShopManagementSystem/src/main/java/com/mycompany/coffeeshopmanagementsystem/CoffeeShopManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coffeeshopmanagementsystem;
+
+/**
+ *
+ * @author Jayjay
+ */
+public class CoffeeShopManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
